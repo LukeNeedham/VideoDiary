2cda4f221431d4fc5d3458d732c40fadcde17d50
Luke <lukeneedham@outlook.com>
2025-01-03 16:22:02 +0100

    Fix FileProviderName

--- app/src/main/java/com/lukeneedham/videodiary/ui/fileprovider/FileProviderName.kt
+++ app/src/main/java/com/lukeneedham/videodiary/ui/fileprovider/FileProviderName.kt
@@ -4,5 +4,5 @@ import com.lukeneedham.videodiary.BuildConfig
 
 object FileProviderName {
     /** Must match the value defined for `FileProvider` the Manifest */
-    const val authority = BuildConfig.APPLICATION_ID
+    const val authority = BuildConfig.APPLICATION_ID + ".provider"
 }
